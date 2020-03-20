8ed64df2bf3463bc16c0cbc6eb7dc1a4f60d917a
Fawez Essayes <58982115+fawezessayes@users.noreply.github.com>
2020-03-20 16:46:46 +0100

    just updated i added redux all ok

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,21 @@
 // packages
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+
 //components
+import { changeSearchFeild } from "../redux/reducers";
 import Scroll from '../components/Scroll'
 import { robots } from '../components/Robots';
 import Cards from '../components/Cards'
 import Search from '../components/Search';
 
+// code
+const mapStateToProps = state =>({
+    searchFeild: changeSearchFeild.searchFeild
+})
+const mapDispatchToProps = dispatch =>({
+    onSearch: event => dispatch(changeSearchFeild(event.target.value))
+})
 class App extends Component{
     constructor(props){
         super(props);
@@ -36,4 +45,4 @@ class App extends Component{
         )
     }
 }
-export default connect()(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
