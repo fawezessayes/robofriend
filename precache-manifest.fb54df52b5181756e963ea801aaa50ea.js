8f42018a388d45249f9d82da9188a2a9b7c1489b
Fawez Essayes <58982115+fawezessayes@users.noreply.github.com>
2020-03-20 18:11:23 +0100

    Updates

---
+++ precache-manifest.fb54df52b5181756e963ea801aaa50ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a17033efaa772458e47b4666e375725",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "5966c6d093a31b72d2d5",
+    "url": "/robofriend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4e8bf905c89af593ed65",
+    "url": "/robofriend/static/css/main.8b8c6319.chunk.css"
+  },
+  {
+    "revision": "5966c6d093a31b72d2d5",
+    "url": "/robofriend/static/js/2.5520d2eb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/robofriend/static/js/2.5520d2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8bf905c89af593ed65",
+    "url": "/robofriend/static/js/main.9ee585b8.chunk.js"
+  },
+  {
+    "revision": "2b24fef87f81acf7cb30",
+    "url": "/robofriend/static/js/runtime-main.9c2acacb.js"
+  }
+]);
